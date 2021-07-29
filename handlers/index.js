9b19de080db25dc2f3438545c79a618ec245cd94
sdoperry <78126970+sdoperry@users.noreply.github.com>
2021-07-28 19:22:43 -0500

    changes

---
+++ handlers/index.js
@@ -0,0 +1,5 @@
+const homeHandler = require("./home");
+const staticHandler = require("./static-handler");
+const catHandler = require("./cat");
+
+module.exports = [homeHandler, staticHandler, catHandler];
---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const port = 3030;
+const handlers = require("./handlers");
+
+http.createServer((req, res) => {
+	for (let handler of handlers) {
+		if (!handler(req, res)) {
+			break;
+		}
+	}
+}).listen(port);
